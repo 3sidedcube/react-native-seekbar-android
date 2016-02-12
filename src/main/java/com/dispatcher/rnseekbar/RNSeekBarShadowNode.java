784124087167bcbdcdd126c362e8f4c6a62413b4
Tim Matthews <tim@3sidedcube.com>
2016-02-12 11:49:50 +0000

    Updgraded to ReactNative 0.20.0

--- src/main/java/com/dispatcher/rnseekbar/RNSeekBarShadowNode.java
+++ src/main/java/com/dispatcher/rnseekbar/RNSeekBarShadowNode.java
@@ -1,11 +1,5 @@
 package com.dispatcher.rnseekbar;
 
-import javax.annotation.Nullable;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import android.util.SparseIntArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
@@ -13,25 +7,23 @@ import android.widget.SeekBar;
 import com.facebook.csslayout.CSSNode;
 import com.facebook.csslayout.MeasureOutput;
 import com.facebook.react.uimanager.LayoutShadowNode;
-import com.facebook.react.uimanager.ReactProp;
 
 /**
  * Node responsible for holding the style of the ProgressBar, see under
  * {@link android.R.attr.progressBarStyle} for possible styles. ReactProgressBarViewManager
  * manages how this style is applied to the ProgressBar.
  */
-public class RNSeekBarShadowNode extends LayoutShadowNode implements CSSNode.MeasureFunction {  
+public class RNSeekBarShadowNode extends LayoutShadowNode implements CSSNode.MeasureFunction {
 
   private Integer mHeight = -1;
   private Integer mWidth = -1;
-  
 
   public RNSeekBarShadowNode() {
     setMeasureFunction(this);
   }
 
   @Override
-  public void measure(CSSNode node, float width, MeasureOutput measureOutput) {    
+  public void measure(CSSNode node, float width, float height, MeasureOutput measureOutput) {
     if (mWidth == -1) {
       SeekBar seekBar = new SeekBar(getThemedContext(), null);
       final int spec = View.MeasureSpec.makeMeasureSpec(
@@ -39,7 +31,7 @@ public class RNSeekBarShadowNode extends LayoutShadowNode implements CSSNode.Mea
           View.MeasureSpec.UNSPECIFIED);
       seekBar.measure(spec, spec);
       mHeight = seekBar.getMeasuredHeight();
-      mWidth = seekBar.getMeasuredWidth();     
+      mWidth = seekBar.getMeasuredWidth();
     }
 
     measureOutput.height = mHeight;
